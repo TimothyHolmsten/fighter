1b159c610162545275838a4f0c293e4c6a0426b0
timothy <timhol@kth.se>
2016-10-13 15:34:22 +0200

    Added bad AI

--- src/model/AI.java
+++ src/model/AI.java
@@ -3,5 +3,25 @@ package model;
 /**
  * Created by timothy on 2016-10-11.
  */
-public class AI {
+public class AI extends Player {
+    private Player enemyPlayer;
+
+    public AI(double x, double y, double width, double height, Player enemyPlayer) {
+        super(x, y, width, height);
+        this.enemyPlayer = enemyPlayer;
+    }
+
+    public void think() {
+        double diffX = enemyPlayer.getX() - getX();
+
+        if (getY() <= enemyPlayer.getY()) {
+            walk(enemyPlayer.getX() - getX());
+        }
+        else if(Math.abs(diffX) > 10)
+            walk( diffX - (1000/diffX) );
+
+
+
+
+    }
 }
