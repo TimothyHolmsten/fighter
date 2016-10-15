2c784cc87cf5ffc5bad095c85176458ff26d3d26
Robert Alm Nilsson <rorialni@gmail.com>
2016-10-15 16:34:33 +0200

    Comment WorldModel

--- src/model/WorldModel.java
+++ src/model/WorldModel.java
@@ -3,15 +3,24 @@ package model;
 import java.util.ArrayList;
 
 /**
- * Created by timothy on 2016-10-11.
+ * This class represents the model of a game world. The {@link #play}
+ * method should be called every frame.
  */
 public class WorldModel {
+    /** The time left before the game ends. */
     public int timeLeft;
+
     private Player player1;
     private Player player2;
     private long endTime;
     private State state = State.RUNNING;
 
+    /**
+     * Constructs a new world model containing the two specified
+     * players.
+     * @param player1 the first player
+     * @param player2 the second player
+     */
     public WorldModel(Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
@@ -20,14 +29,26 @@ public class WorldModel {
         timeLeft = (int) (endTime - System.currentTimeMillis());
     }
 
+    /**
+     * Returns the first player.
+     * @return the first player
+     */
     public Player getPlayer1() {
         return player1;
     }
 
+    /**
+     * Returns the second player.
+     * @return the second player
+     */
     public Player getPlayer2() {
         return player2;
     }
 
+    /**
+     * Returns a list of all players.
+     * @return a list of all players
+     */
     public ArrayList<Player> getPlayers() {
         ArrayList<Player> players = new ArrayList<>();
         players.add(player1);
@@ -36,14 +57,29 @@ public class WorldModel {
         return players;
     }
 
+    /**
+     * Returns the state of the game.
+     * @return the state of the game
+     */
     public State getState() {
         return state;
     }
 
+    /**
+     * Sets the state of the game.
+     * @param state the new state of the game
+     */
     public void setState(State state) {
         this.state = state;
     }
 
+    /**
+     * Updates the game. This method should be called every frame.
+     * @param time the elapsed time since the last time this method
+     * was called
+     * @param boxX the width of the game world
+     * @param boxY the height of the game world
+     */
     public void play(long time, double boxX, double boxY) {
         if (getState() == State.RUNNING) {
             timeLeft = (int) (endTime - System.currentTimeMillis());
